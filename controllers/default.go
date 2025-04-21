84a11699ea93b5dd1707514751fddf81f21f043a
trando1204 <hungvuvan.jpit@gmail.com>
2025-04-21 23:59:40 +0700

    support for mexc

--- controllers/default.go
+++ controllers/default.go
@@ -12,6 +12,7 @@ type MainController struct {
 }
 
 var BinanceAPI = "https://api.binance.com/api/v3/"
+var MexcAPI = "https://api.mexc.com/api/v3/"
 
 func (this *MainController) GetBinancePrice() {
 	var symbol string
@@ -95,6 +96,95 @@ func (this *MainController) GetBinanceCandleStick() {
 	if err := services.HttpRequest(req, &result); err != nil {
 		logpack.Error("Get Binance Candle Stick error", "GetBinanceCandleStick")
 		this.Data["json"] = map[string]string{"status": "false", "errMsg": "Get Binance Candle Stick data failed"}
+		this.ServeJSON()
+		return
+	}
+	this.Data["json"] = result
+	this.ServeJSON()
+}
+
+func (this *MainController) GetMexcPrice() {
+	var symbol string
+	if err := this.Ctx.Input.Bind(&symbol, "symbol"); err != nil {
+		logpack.Error(err.Error(), "GetMexcPrice")
+		this.Data["json"] = map[string]string{"status": "false", "errMsg": "Get Symbol failed"}
+		this.ServeJSON()
+		return
+	}
+	query := map[string]string{
+		"symbol": symbol,
+	}
+	url := fmt.Sprintf("%sticker/24hr", MexcAPI)
+	req := &services.ReqConfig{
+		Method:  http.MethodGet,
+		HttpUrl: url,
+		Payload: query,
+	}
+	var result interface{}
+	if err := services.HttpRequest(req, &result); err != nil {
+		this.Data["json"] = map[string]string{"status": "false", "errMsg": "Get Mexc Price data failed"}
+		this.ServeJSON()
+		return
+	}
+	this.Data["json"] = result
+	this.ServeJSON()
+}
+
+func (this *MainController) GetMexcDepth() {
+	var symbol, limit string
+	symbolErr := this.Ctx.Input.Bind(&symbol, "symbol")
+	limitErr := this.Ctx.Input.Bind(&limit, "limit")
+	if symbolErr != nil || limitErr != nil {
+		logpack.Error("Get param error", "GetMexcDepth")
+		this.Data["json"] = map[string]string{"status": "false", "errMsg": "Get Param failed"}
+		this.ServeJSON()
+		return
+	}
+	query := map[string]string{
+		"symbol": symbol,
+		"limit":  limit,
+	}
+	url := fmt.Sprintf("%sdepth", MexcAPI)
+	req := &services.ReqConfig{
+		Method:  http.MethodGet,
+		HttpUrl: url,
+		Payload: query,
+	}
+	var result interface{}
+	if err := services.HttpRequest(req, &result); err != nil {
+		logpack.Error("Get Mexc Depth error", "GetMexcDepth")
+		this.Data["json"] = map[string]string{"status": "false", "errMsg": "Get Mexc Depth data failed"}
+		this.ServeJSON()
+		return
+	}
+	this.Data["json"] = result
+	this.ServeJSON()
+}
+
+func (this *MainController) GetMexcCandleStick() {
+	var symbol, interval string
+	symbolErr := this.Ctx.Input.Bind(&symbol, "symbol")
+	intervalErr := this.Ctx.Input.Bind(&interval, "interval")
+	if symbolErr != nil || intervalErr != nil {
+		logpack.Error("Get param error", "GetMexcCandleStick")
+		this.Data["json"] = map[string]string{"status": "false", "errMsg": "Get Param failed"}
+		this.ServeJSON()
+		return
+	}
+	query := map[string]string{
+		"symbol":   symbol,
+		"interval": interval,
+	}
+	url := fmt.Sprintf("%sklines", MexcAPI)
+	req := &services.ReqConfig{
+		Method:  http.MethodGet,
+		HttpUrl: url,
+		Payload: query,
+	}
+	var result interface{}
+	if err := services.HttpRequest(req, &result); err != nil {
+		logpack.Error("Get Mexc Candle Stick error", "GetMexcCandleStick")
+		this.Data["json"] = map[string]string{"status": "false", "errMsg": "Get Mexc Candle Stick data failed"}
 		this.ServeJSON()
 		return
 	}
--- routers/router.go
+++ routers/router.go
@@ -10,4 +10,7 @@ func init() {
 	beego.Router("/api/v3/ticker/24hr", &controllers.MainController{}, "get:GetBinancePrice")
 	beego.Router("/api/v3/depth", &controllers.MainController{}, "get:GetBinanceDepth")
 	beego.Router("/api/v3/klines", &controllers.MainController{}, "get:GetBinanceCandleStick")
+	beego.Router("/api/mexc/v3/ticker/24hr", &controllers.MainController{}, "get:GetMexcPrice")
+	beego.Router("/api/mexc/v3/depth", &controllers.MainController{}, "get:GetMexcDepth")
+	beego.Router("/api/mexc/v3/klines", &controllers.MainController{}, "get:GetMexcCandleStick")
 }
